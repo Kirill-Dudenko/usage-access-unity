4464a177d61ad0dfd2f8068347b01bff847e7e01
kdudenko <k.dudenko@tg.dunice.net>
2022-11-08 14:40:53 +0300

    redesign

--- app/src/main/java/com/boints/usagestats/MainActivity.kt
+++ app/src/main/java/com/boints/usagestats/MainActivity.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -37,15 +39,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("Access granted: $accessGranted")
+                    Column{
+                        Text("Access granted: $accessGranted")
+                        Button(
+                            onClick = {
+                                showDialog()
+                            }
+                        ){
+                            Text("Open settings")
+                        }
+                    }
+
                 }
             }
         }
-        showDialog()
     }
 
     fun showDialog() {
         val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+            .putExtra("android.provider.extra.APP_PACKAGE", packageName)
+
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         startActivityForResult(intent, 0)
     }
