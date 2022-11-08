0d79f6a6dad3879bcdbc112ff3910f013e4d99e0
kdudenko <k.dudenko@tg.dunice.net>
2022-11-08 17:02:40 +0300

    watching for access granted

--- app/src/main/java/com/boints/usagestats/MainActivity.kt
+++ app/src/main/java/com/boints/usagestats/MainActivity.kt
@@ -5,22 +5,16 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Process
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
 import com.boints.usagestats.ui.theme.UsageStatsTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -28,12 +22,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launchWhenStarted {
-            while (!accessGranted) {
-                accessGranted = isAccessGranted()
-                delay(500)
-            }
-        }
+
+        watchForUsageStatsPermission()
         setContent {
             UsageStatsTheme {
                 Surface(
@@ -44,26 +34,60 @@ class MainActivity : ComponentActivity() {
                         Text("Access granted: $accessGranted")
                         Button(
                             onClick = {
-                                showDialog()
+                                showUsageStatsPermissionDialog()
                             }
                         ) {
                             Text("Open settings")
                         }
                     }
-
                 }
             }
         }
     }
 
-    fun showDialog(specifyPackage: Boolean = true) {
+    fun showUsageStatsPermissionDialog(specifyPackage: Boolean = true) {
         val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
 
         if (specifyPackage)
             intent.data = Uri.parse("package:$packageName")
 
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivityForResult(intent, 0)
+        //intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent,)
+    }
+
+    private fun watchForUsageStatsPermission(): Unit {
+        val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(
+            AppOpsManager.OPSTR_GET_USAGE_STATS,
+            Process.myUid(), packageName
+        )
+
+        if (mode == AppOpsManager.MODE_ALLOWED) {
+            accessGranted = true
+            return
+        }
+
+        fun watcher(op: String, packageName: String) {
+            val mode = appOps.checkOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(), getPackageName()
+            )
+            if (mode != AppOpsManager.MODE_ALLOWED) {
+                accessGranted = false
+                return
+            }
+            appOps.stopWatchingMode(::watcher)
+            accessGranted = true
+            val intent = Intent(this@MainActivity, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            applicationContext.startActivity(intent)
+        }
+
+        appOps.startWatchingMode(
+            AppOpsManager.OPSTR_GET_USAGE_STATS,
+            applicationContext.packageName,
+            ::watcher
+        )
     }
 
     private fun isAccessGranted(): Boolean {
